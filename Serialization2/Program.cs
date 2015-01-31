a8bb0585b6438511ae1ada7b3214101bf230097f
beysenbek <beysenbek@gmail.com>
2015-02-01 02:05:34 +0400

    Samples from Lesson6

---
+++ Serialization2/Program.cs
@@ -0,0 +1,36 @@
+﻿using Serialization1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person(120);
+            p.Name = "Bill";
+            p.Surname = "John";
+
+            XmlSerializer x = new XmlSerializer(typeof(Person));
+            FileStream fs = new FileStream("t.txt",FileMode.OpenOrCreate);
+
+           // x.Serialize(fs, p);
+
+            fs.Close();
+
+
+            fs = new FileStream("t.txt", FileMode.Open);
+
+            Person p2 = x.Deserialize(fs) as Person;
+
+            fs.Close();
+        }
+    }
+}
---
+++ Serialization3/Program.cs
@@ -0,0 +1,24 @@
+﻿using Serialization1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer x = new XmlSerializer(typeof(Person));
+            FileStream fs = new FileStream("t2.txt", FileMode.Open);
+
+            Person p2 = x.Deserialize(fs) as Person;
+
+            fs.Close();
+        }
+    }
+}
